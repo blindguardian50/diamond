71914bb0d88d9c98ce133afad602010f2ebdad6a
Ludwig <ludwig.reinhardt@student.tugraz.at>
2023-02-06 13:20:10 +0100

    add first click

--- src/app/treetest-components/treetest-tests/treetest-tests.component.ts
+++ src/app/treetest-components/treetest-tests/treetest-tests.component.ts
@@ -286,7 +286,6 @@ export class TreetestTestsComponent implements OnInit {
 
   firstClickStatistic(){
 
-    // Returns the most frequently occuring string in a list.
     function mode(arr){
       return arr.sort((a,b) =>
             arr.filter(v => v===a).length
@@ -298,7 +297,7 @@ export class TreetestTestsComponent implements OnInit {
 
       let firstClicks = [];
 
-      // Get most first clicked node.
+      // Get most first clicked
       for(let j = 0; j < this.paths.length; j++){
         firstClicks.push(this.paths[j].paths[i][0].node.data.text);
       }
@@ -306,7 +305,7 @@ export class TreetestTestsComponent implements OnInit {
       const mostFirstClicked = mode(firstClicks); 
       let firstClickedNrOfTimes = 0;
 
-      // Get nr of clicks for this node name.
+      // Get nr of clicks.
       for(let j = 0; j < this.paths.length; j++){
         if(this.paths[j].paths[i][0].node.data.text === mostFirstClicked){
           firstClickedNrOfTimes += 1;
@@ -315,6 +314,8 @@ export class TreetestTestsComponent implements OnInit {
 
       const percentageFirstClicked = Math.floor((firstClickedNrOfTimes * 100) / this.paths.length);      
 
+      console.log("most first clicked: "  + mostFirstClicked + ", at ", + firstClickedNrOfTimes + " / " + this.paths.length + " times, " + percentageFirstClicked + "%");
+
       // Save first click for this task. 
       // [0] is string, [1] nr of clicks, [2] percentage
       let firstClicksForThisTask = [];
@@ -322,9 +323,12 @@ export class TreetestTestsComponent implements OnInit {
       firstClicksForThisTask.push(firstClickedNrOfTimes);
       firstClicksForThisTask.push(percentageFirstClicked);
 
-      // This container is used by Angular in the frontend.
       this.firstClicks.push(firstClicksForThisTask);
     }
+    
+    console.log(this.firstClicks);
+    
+
   }
 
   preparePathTree(index) {
